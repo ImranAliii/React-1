b20058f73cfae4db0839b88191ce375ef5bda79a
ImranAliii <pakimranaliarain@gmail.com>
2024-10-13 23:43:21 -0700

    firstcommit

---
+++ my_project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navigation from './Components/Navigation'
+import Hero from './Components/Hero'
+
+function App() {
+ 
+
+  return (
+    
+    <>
+    
+    <Navigation/>
+    <Hero/>
+    
+    </>
+  )
+}
+
+export default App
